0605f1f8d3bda9b1d64bd5feb6e94426b6a5fbc8
Luis Beltran <luis.beltran@itcelaya.edu.mx>
2024-08-12 16:45:13 +0200

    Agregar archivos de proyecto.

---
+++ Plugins/CityWeatherPlugin.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.SemanticKernel;
+using System.ComponentModel;
+
+namespace SKReactor.Plugins
+{
+    // Native function: native code with custom logic
+    public class CityWeatherPlugin
+    {
+        HttpClient client = new HttpClient();
+
+        [KernelFunction]
+        [Description("Describes the current weather of a city in JSON format")]
+        public async Task<string> GetWeather(
+            [Description("The name of the city ")] string city)
+        {
+            var key = "";
+            var query = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}";
+
+            var result = await client.GetAsync(query);
+
+            if (result.IsSuccessStatusCode)
+                return await result.Content.ReadAsStringAsync();
+
+            return "Error, the weather of the city is not available at this moment";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.TextToImage;
+using SKReactor.Plugins;
+using System;
+
+var deployment = "";
+var model = "";
+var endpoint = "";
+var key = "";
+
+var builder = Kernel.CreateBuilder();
+
+builder.Services.AddAzureOpenAIChatCompletion(
+    deployment, endpoint, key, model);
+
+#pragma warning disable SKEXP0010
+builder.Services.AddAzureOpenAITextToImage("Dalle3", endpoint, key);
+
+
+builder.Plugins.AddFromType<CityWeatherPlugin>();
+
+
+var kernel = builder.Build();
+
+/*
+var prompt = "Give me a list of 10 cool short names for a software development company.";
+var result = await kernel.InvokePromptAsync(prompt);
+Console.WriteLine(prompt);
+Console.WriteLine(result);
+
+OpenAIPromptExecutionSettings settings =
+    new() { MaxTokens = 100, Temperature = 0.7 };
+KernelArguments arguments = new(settings);
+
+var prompt2 = "Write a summary about Olympic Games in Modern Era";
+var result2 = await kernel.InvokePromptAsync(prompt2, arguments);
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine(prompt2);
+Console.WriteLine(result2);
+
+var result2b = kernel.InvokePromptStreamingAsync(prompt2);
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine(prompt2);
+await foreach (var token in result2b)
+{
+    Console.Write(token);
+}
+
+*/
+
+
+/*
+var prompts = kernel.CreatePluginFromPromptDirectory("Prompts");
+
+var city = "Prague";
+var kargs = new KernelArguments() { { "destination", city } };
+var activities = await kernel.InvokeAsync(
+    prompts["SuggestActivities"], kargs);
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine(city);
+Console.WriteLine(activities);
+
+
+string input = "I want to learn about Semantic Kernel with my friends Pablo and Luis";
+string language = "French";
+KernelArguments arguments = new() { { "input", input }, { "language", language } };
+
+var response = await kernel.InvokeAsync(prompts["GetTranslation"], arguments);
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine(input);
+Console.WriteLine($"{response}");
+
+
+string input2 = "I want to learn about Semantic Kernel with my friends Pablo and Luis";
+KernelArguments arguments2 = new() { { "input", input2 }};
+
+var response2 = await kernel.InvokeAsync(prompts["GetTranslation"], arguments2);
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine(input2);
+Console.WriteLine($"{response2}");
+
+*/
+
+var city4 = "Prague";
+KernelArguments args4 = new() { { "city", city4 } };
+var result4 = await kernel.InvokeAsync("CityWeatherPlugin", "GetWeather", args4);
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine(city4);
+Console.WriteLine(result4);
+
+OpenAIPromptExecutionSettings settings = new()
+{
+    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+};
+KernelArguments args5 = new(settings);
+
+var prompt5 = "What are the current weather conditions in Prague and in Mexico City? " +
+    "I want to know the temperature in Celsius degrees";
+var result5 = await kernel.InvokePromptAsync(prompt5, args5);
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine(prompt5);
+Console.WriteLine(result5);
+
+
+
+var prompt6 = "Is it raining in Guanajuato (in Mexico) or in Rome (in Italy) right now?";
+var result6 = await kernel.InvokePromptAsync(prompt6, args5);
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine(prompt6);
+Console.WriteLine(result6);
+
+
+/*
+#pragma warning disable SKEXP0001
+var dallE = kernel.GetRequiredService<ITextToImageService>();
+var imageDescription = "A 3D fluffy tuxedo cat wearing a tie and a hat";
+
+var imageUrl = await dallE.GenerateImageAsync(imageDescription, 1024, 1024);
+Console.WriteLine(imageDescription);
+Console.WriteLine(imageUrl);
+
+*/
